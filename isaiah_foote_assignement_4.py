d25e9bd90384253419c5708ce55ba5fb57af17cd
IJFOOTE <isaiahfoote985@gmail.com>
2025-09-24 16:16:43 -0400

    Pass Test Case 1: Initial game setup with required variables"

---
+++ isaiah_foote_assignement_4.py
@@ -0,0 +1,7 @@
+student_name = "Isaiah"
+current_gpa = 3.9
+study_hours = 3
+social_points = 24
+stress_level = 100
+print(f"welcome this is your starting stats{student_name, current_gpa,study_hours,social_points,stress_level}")
+git commit -m "Pass Test Case 1: Initial game setup with required variables"
